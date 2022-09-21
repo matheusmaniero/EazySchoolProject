78c6359ffdfc3c6c99e64e0de985512e29f5dc99
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-21 19:42:57 -0300

    First Example Using ThymeLeaf and Multiple Paths In @RequestMapping

---
+++ src/main/java/com/eazySchoolProject/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.eazySchoolProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value= {"","/","home","home.html"})
+	public String displayHomePage(Model model) {
+		model.addAttribute("username", "Matheus");
+		return "home.html";
+	}
+
+}
