44a4534f6e2cb5c54d910d22b31ab64750a31774
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-26 18:10:24 -0300

    Courses page added and example of WebMvcConfigurer inteface

---
+++ src/main/java/com/eazySchoolProject/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.eazySchoolProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	
+	/*
+	 * 
+	 * since we are creating static pages, without any business logic we can simple
+	 * use this Interface to avoid creating unnecessary view controllers. 
+	 * 
+	 */
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		
+		registry.addViewController("/courses").setViewName("courses");
+		
+		
+	}
+	
+	
+	
+	
+}
