f05ddba9dda74a0fa08d13887637ea19c62531f6
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-26 19:39:20 -0300

    Backend is recieving data from contact page

--- src/main/java/com/eazySchoolProject/controller/ContactController.java
+++ src/main/java/com/eazySchoolProject/controller/ContactController.java
@@ -1,16 +1,41 @@
 package com.eazySchoolProject.controller;
 
+import java.util.logging.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
-	
+	private static   org.slf4j.Logger log =  LoggerFactory.getLogger(ContactController.class);
 	
 	@RequestMapping(value= {"/contact"})
 	public String displayContactPage() {
 		return "contact.html";
 	}
+	
+	@PostMapping(value="/saveMsg")
+	public ModelAndView saveMessage(@RequestParam String name, @RequestParam String mobileNum, @RequestParam String
+			email, @RequestParam String subject, @RequestParam String message) {
+		
+		log.info("Name: " + name);
+		log.info("Mobile Number: " + mobileNum);
+		log.info("Email: " + email);
+		log.info("Subject: " + subject);
+		log.info("Message: " + message);
+		return new ModelAndView("redirect:/contact");
+		
+		
+		
+		
+	}
+	
+	
+	
 	
 
 }
