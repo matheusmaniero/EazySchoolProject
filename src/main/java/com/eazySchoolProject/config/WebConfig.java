3924180df4c5229689fa4b606f6135864243e11d
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-26 18:38:33 -0300

    Added About Page

--- src/main/java/com/eazySchoolProject/config/WebConfig.java
+++ src/main/java/com/eazySchoolProject/config/WebConfig.java
@@ -18,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		
 		registry.addViewController("/courses").setViewName("courses");
+		registry.addViewController("/about").setViewName("about");
 		
 		
 	}
