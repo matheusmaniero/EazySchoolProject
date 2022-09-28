a6281e9e76bb9ef36e0549c587cae47d309614ec
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-28 19:45:15 -0300

    Server-side Form Validatitions implemented

--- src/main/java/com/eazySchoolProject/controller/ContactController.java
+++ src/main/java/com/eazySchoolProject/controller/ContactController.java
@@ -1,15 +1,22 @@
 package com.eazySchoolProject.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.eazySchoolProject.model.Contact;
 import com.eazySchoolProject.service.ContactService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class ContactController {
 	
 	private final ContactService cs;
@@ -20,16 +27,26 @@ public class ContactController {
 	}
 	
 	@RequestMapping(value= {"/contact"})
-	public String displayContactPage() {
-		return "contact.html";
+	public String displayContactPage(Model model) {
+		model.addAttribute("contact", new Contact());
+		
+		return "contact";
 	}
 	
 	@PostMapping(value="/saveMsg")
-	public ModelAndView saveMessage(Contact contact) {
+	public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+		
+		if (errors.hasErrors()) {
+			
+			log.error("Contact form validation failed due to: "+ errors.toString());
+			return "contact";
+			
+			
+		}
 		
 		cs.saveContent(contact);
 		
-		return new ModelAndView("redirect:/contact");
+		return "redirect:/contact";
 		
 		
 		
