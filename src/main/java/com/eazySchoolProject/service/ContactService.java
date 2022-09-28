62858a2eea7a31ebd5a06dbe632a97db4750ebc3
Matheus Maniero <matheusmaniero@gmail.com>
2022-09-28 20:54:10 -0300

    @RequestScope example in ContactService Class

--- src/main/java/com/eazySchoolProject/service/ContactService.java
+++ src/main/java/com/eazySchoolProject/service/ContactService.java
@@ -1,6 +1,8 @@
 package com.eazySchoolProject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.RequestScope;
 
 import com.eazySchoolProject.model.Contact;
 
@@ -8,11 +10,19 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@RequestScope
 public class ContactService {
 	
+	private int counter = 0;
+	
+	public ContactService() {
+		System.out.println("ContactService is been initialized");
+	}
 	
 	public boolean saveContent(Contact contact) {
 		log.info(contact.toString());
+		this.counter += 1;
+		System.out.println(counter);
 		return true;
 		
 	}
