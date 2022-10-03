90bdc063631467da756a8301a2b46c937f3f3c28
Matheus Maniero <matheusmaniero@gmail.com>
2022-10-03 18:06:59 -0300

    Using InMemoryUserDetailsManager to add users into the application

--- src/main/java/com/eazySchoolProject/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazySchoolProject/config/ProjectSecurityConfig.java
@@ -3,6 +3,9 @@ package com.eazySchoolProject.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +19,7 @@ public class ProjectSecurityConfig {
 		 * 
 		 */
 		http.csrf().disable().authorizeRequests()
-		.mvcMatchers("/home").permitAll()
+		.mvcMatchers("/home").authenticated()
 		.mvcMatchers("/holidays/**").permitAll()
 		.mvcMatchers("/contact").permitAll()
 		.mvcMatchers("/saveMsg").permitAll()
@@ -42,5 +45,37 @@ public class ProjectSecurityConfig {
 		
 	
 	}
+	
+	
+	@Bean
+	public InMemoryUserDetailsManager userDetailService() {
+		
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("12345")
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("54321")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user,admin);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
