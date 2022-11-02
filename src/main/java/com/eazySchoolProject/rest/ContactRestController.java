41e93b8a71584608f5538b2777803afd994343df
Matheus Maniero <matheusmaniero@gmail.com>
2022-11-02 17:51:56 -0300

    Delete Messages Feature Using API Implemented

--- src/main/java/com/eazySchoolProject/rest/ContactRestController.java
+++ src/main/java/com/eazySchoolProject/rest/ContactRestController.java
@@ -1,12 +1,16 @@
 package com.eazySchoolProject.rest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -59,6 +63,26 @@ public class ContactRestController {
 		response.setStatusMsg("Message saved successfully");
 		return ResponseEntity.status(HttpStatus.CREATED).header("isMsgSaved", "true").body(response);
 			
+	}
+	
+	@DeleteMapping("/deleteMsg")
+	public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity){
+		
+		HttpHeaders headers = requestEntity.getHeaders();
+		
+		headers.forEach((key,value) -> {
+			
+			log.info(String.format("Header '%s' = %s",key,value.stream().collect(Collectors.joining("|"))));
+			
+		});
+		
+		Contact contact = requestEntity.getBody();
+		contactRepository.deleteById(contact.getContactId());
+		Response response = new Response();
+		response.setStatusCode("200");
+		response.setStatusMsg("Message Successfully Deleted");
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+		
 	}
 	
 	
