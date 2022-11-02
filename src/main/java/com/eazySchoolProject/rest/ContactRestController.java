2862f05b6467207bc348e0bc2c97a2568ca99492
Matheus Maniero <matheusmaniero@gmail.com>
2022-11-02 16:53:44 -0300

    Saving a Message Sending Through The API in JSON Format

--- src/main/java/com/eazySchoolProject/rest/ContactRestController.java
+++ src/main/java/com/eazySchoolProject/rest/ContactRestController.java
@@ -2,21 +2,27 @@ package com.eazySchoolProject.rest;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.eazySchoolProject.model.Contact;
+import com.eazySchoolProject.model.Response;
 import com.eazySchoolProject.repository.ContactRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping(path="/api/contact")
 public class ContactRestController {
 	
@@ -24,7 +30,7 @@ public class ContactRestController {
 	private ContactRepository contactRepository;
 	
 	@GetMapping("/getMessagesByStatus")
-	@ResponseBody
+	//@ResponseBody
 	public List<Contact> getMessagesByStatus(@RequestParam(name="status") String status){
 		
 		return contactRepository.findByStatus(status);
@@ -32,7 +38,7 @@ public class ContactRestController {
 	}
 	
 	@GetMapping("/getAllMessagesByStatus")
-	@ResponseBody
+	//@ResponseBody
 	public List<Contact> getAllMessagesByStatus(@RequestBody Contact contact){
 		
 		if (contact != null && contact.getStatus() != null) {
@@ -40,6 +46,19 @@ public class ContactRestController {
 		}
 		
 		return List.of();
+	}
+	
+	@PostMapping("/saveMsg")
+	public ResponseEntity<Response> saveMessages(@RequestHeader("invocationFrom") String invocationFrom,
+			@Valid @RequestBody Contact contact){
+		
+		log.info(String.format("Header invocation from: %s", invocationFrom));
+		contactRepository.save(contact);
+		Response response = new Response();
+		response.setStatusCode("200");
+		response.setStatusMsg("Message saved successfully");
+		return ResponseEntity.status(HttpStatus.CREATED).header("isMsgSaved", "true").body(response);
+			
 	}
 	
 	
